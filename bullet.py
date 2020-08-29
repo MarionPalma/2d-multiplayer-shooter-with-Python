f88ff430aae8f29e1eb3c1d271ced9fbcf869d4b
MarionPalma <marion_rozen@hotmail.com>
2020-08-29 20:05:08 +0200

    Add files via upload

---
+++ bullet.py
@@ -0,0 +1,11 @@
+class Bullet:
+    x = 0
+    y = 0
+    state = "ready"
+
+    def __init__(self, x):
+        self.x = x
+
+    def draw(self, screen, bulletimage, y):
+        self.y = y
+        screen.blit(bulletimage, (self.x, self.y))
---
+++ player.py
@@ -0,0 +1,22 @@
+class Player:
+
+    x = 0
+    y = 0
+
+    def __init__(self, x, y):
+       self.x = x
+       self.y = y
+
+    def draw(self, screen, image):
+        screen.blit(image, (self.x, self.y))
+
+    def boundaries(self, y):
+        if y <= 0:
+            self.y = 0
+        if y >= 436:
+            self.y = 436
+
+
+
+
+
